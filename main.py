445d63ed749626da811e19fef15da30b168fac9d
Quratul Ain <107280962+QAin-codes@users.noreply.github.com>
2022-08-05 15:27:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+raining=input("is it raining ? ")
+raining=str.lower(raining)
+if raining == "yes":
+  print("take umberella with you..")
+else:
+  print("enjoy your day ")
+  windy =input("is it windy ? ")
+  windy =str.lower(windy)
+  if windy =="yes":
+    print("it is too windy for umberella")
+  else:
+    print("have a good day")
